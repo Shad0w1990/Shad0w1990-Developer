e27321616a4bc2ed2b2a1a12a3cfbdd70dd00aa0
Ehsan Khorsand <50406369+Shad0w1990@users.noreply.github.com>
2019-05-08 17:05:30 +0430

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -17,6 +17,8 @@ namespace GitTest
             CreateWebHostBuilder(args).Build().Run();
         }
 
+        
+        
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
